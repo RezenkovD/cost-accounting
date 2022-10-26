58cfd46ba75d9fbf4d3cc5b2540fbbc793ab9945
RezenkovD <rezenkovdmitro@gmail.com>
2022-10-26 23:47:42 +0300

    add parameter nullable=True in hashed_password

--- models/user.py
+++ models/user.py
@@ -9,6 +9,6 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    hashed_password = Column(String, nullable=True)
 
     items = relationship("Item", back_populates="user")
