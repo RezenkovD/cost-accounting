a1bff076a4b5411e3bb588dc8c6cc4b1816455fd
RezenkovD <rezenkovdmitro@gmail.com>
2022-10-28 09:53:52 +0300

    Delete redundant functionality (get item)

--- crud/crud_item.py
+++ crud/crud_item.py
@@ -3,10 +3,6 @@ from schemas.item import ItemCreate
 import models.item
 
 
-def get_item(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.item.Item).offset(skip).limit(limit).all()
-
-
 def create_user_item(db: Session, item: ItemCreate, user_id: int):
     db_item = models.item.Item(**item.dict())
     db_item.user_id = user_id
