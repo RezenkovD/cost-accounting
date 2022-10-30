e20b2c0a3da3eb842b5e50c74d01daf38ee01347
RezenkovD <rezenkovdmitro@gmail.com>
2022-10-30 09:14:23 +0200

    Add nullable=False in User model

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String, nullable=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
 
     items = relationship("Item", back_populates="user")
