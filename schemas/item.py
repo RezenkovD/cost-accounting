a80284ea480627578d4f7c14151389a886fcbd1f
RezenkovD <rezenkovdmitro@gmail.com>
2022-11-02 17:43:05 +0200

    Add validate for category

--- schemas/item.py
+++ schemas/item.py
@@ -1,11 +1,24 @@
+from enum import Enum
+
 from pydantic import BaseModel
 from pydantic.schema import datetime
+
+
+class CategoryEnum(str, Enum):
+    books = "Books"
+    cafes_and_restaurants = "Cafes and restaurants"
+    travels = "Travels"
+    products_and_supermarkets = "Products and supermarkets"
+    household_appliances = "Household appliances"
+    transfer_to_the_card = "Transfer to the card"
+    others = "Others"
 
 
 class ItemBase(BaseModel):
     description: str
     price: float
     time: datetime
+    category: CategoryEnum
 
 
 class ItemCreate(ItemBase):
