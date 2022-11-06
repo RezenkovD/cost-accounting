9601fbd3401835988f3467f7040bdefd5c45556f
RezenkovD <rezenkovdmitro@gmail.com>
2022-11-06 12:49:59 +0200

    Correct create item

--- crud/crud_item.py
+++ crud/crud_item.py
@@ -13,11 +13,18 @@ def create_user_item(db: Session, item: ItemCreate, user_id: int):
         raise HTTPException(status_code=404, detail="User not found")
     db_item = models.item.Item(**item.dict())
     db_item.user_id = user_id
-    user = db.query(User).join(Category, Category.user_id == User.id).filter(Category.id == db_item.category_id).all()
-    if user is not None:
-        db.add(db_item)
-        db.commit()
-        db.refresh(db_item)
-        return db_item
+
+    users = db.query(User).join(Category, Category.user_id == User.id).filter(Category.id == db_item.category_id).all()
+    if len(users) == 0:
+        raise HTTPException(status_code=404, detail="Invalid Category ID. None of the users have it.")
+    elif len(users) > 1:
+        raise HTTPException(status_code=404, detail="Several users have the same category.")
     else:
-        raise HTTPException(status_code=404, detail="You have no such category")
+        user = users[0]
+        if user.id == db_item.user_id:
+            db.add(db_item)
+            db.commit()
+            db.refresh(db_item)
+            return db_item
+        else:
+            raise HTTPException(status_code=404, detail="The user is not the owner of this category.")
