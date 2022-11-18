8ffa5ea27e789c232775ff0ad57c1f73f7fd6f43
RezenkovD <rezenkovdmitro@gmail.com>
2022-11-18 09:05:20 +0200

    DRY. Create contest.py

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,22 +1,4 @@
-import pytest
-from fastapi.testclient import TestClient
-
-from main import app
-from db.database import get_db
-from db.database import Base
-from db.test_database import engine, override_get_db
-
-
-@pytest.fixture()
-def test_db():
-    Base.metadata.create_all(bind=engine)
-    yield
-    Base.metadata.drop_all(bind=engine)
-
-
-app.dependency_overrides[get_db] = override_get_db
-
-client = TestClient(app)
+from tests.contest import client, test_db
 
 
 def test_get_stats(test_db):
