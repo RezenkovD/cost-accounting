2369cb316809b63b6813275b3b3b9bbb8720a235
Oleksii Hlavatskyi <qqlexa@flyaps.com>
2022-11-26 14:19:13 +0200

    Fix import settings

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.db.database import Base
-from config import settings
+from app.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
