678d4e021e6f21f4488a1b4da6f17da52e86af43
RezenkovD <rezenkovdmitro@gmail.com>
2022-12-08 14:00:19 +0200

    Add new endpoint with get data about current user and functionality for this

--- src/app/crud/crud_user.py
+++ src/app/crud/crud_user.py
@@ -1,7 +1,12 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
 from sqlalchemy.orm import Session
+from starlette import status
 
-from app.schemas import UserCreate
+from app.crud.crud_token import SECRET_KEY, ALGORITHM
+from app.db import get_db
+from app.schemas import UserCreate, TokenData
 from app.models import User
 from app.utils import get_password_hash, verify_password
 
@@ -37,3 +42,29 @@ def authenticate_user(db: Session, email: str, password: str):
     if not verify_password(password, db_user.hashed_password):
         raise HTTPException(status_code=404, detail="Invalid password")
     return db_user
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user_username(db, email=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    return current_user
--- src/app/routers/users.py
+++ src/app/routers/users.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 
 from app import schemas
 from app.crud import crud_user
+from app.crud.crud_user import get_current_active_user
 from app.db import get_db
 
 router = APIRouter(
@@ -31,3 +32,8 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
+
+@router.get("/me/", response_model=schemas.User)
+async def read_users_me(current_user: schemas.User = Depends(get_current_active_user)):
+    return current_user
