a481354d445036ab3346672e67aee9dbd220feba
RezenkovD <rezenkovdmitro@gmail.com>
2022-12-08 12:07:04 +0200

    Add function 'create_access_token'

---
+++ src/app/crud/crud_token.py
@@ -0,0 +1,18 @@
+import os
+from datetime import datetime, timedelta
+
+from jose import jwt
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
