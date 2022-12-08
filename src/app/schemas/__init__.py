bb7c9e06e7dc05f1adda4e1e2634f0d46019982f
RezenkovD <rezenkovdmitro@gmail.com>
2022-12-08 11:46:29 +0200

    Add Token and TokenData schemas

--- src/app/schemas/__init__.py
+++ src/app/schemas/__init__.py
@@ -2,3 +2,4 @@ from .category import Category, CategoryCreate
 from .item import Item, ItemCreate
 from .statistics import Statistics
 from .user import User, UserCreate
+from .token import Token, TokenData
