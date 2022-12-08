203a3f60e21db430a95245024be5313fee13e2f8
RezenkovD <rezenkovdmitro@gmail.com>
2022-12-08 12:39:29 +0200

    Add new functions 'authenticate_user' and 'get_user_username'

--- src/app/crud/__init__.py
+++ src/app/crud/__init__.py
@@ -1,5 +1,11 @@
 from .crud_category import create_user_category
 from .crud_item import create_user_item
 from .crud_statistics import get_user_statistics
-from .crud_user import get_user, get_user_by_email, get_users, create_user
-from .crud_token import create_access_token
+from .crud_user import (
+    get_user,
+    get_user_by_email,
+    get_users,
+    create_user,
+    authenticate_user,
+)
+from .crud_token import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
--- src/app/crud/crud_user.py
+++ src/app/crud/crud_user.py
@@ -1,8 +1,9 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from app.schemas import UserCreate
 from app.models import User
-from app.utils import get_password_hash
+from app.utils import get_password_hash, verify_password
 
 
 def get_user(db: Session, user_id: int):
@@ -22,4 +23,17 @@ def create_user(db: Session, user: UserCreate):
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
+    return db_user
+
+
+def get_user_username(db: Session, email: str):
+    return db.query(User).filter_by(email=email).one_or_none()
+
+
+def authenticate_user(db: Session, email: str, password: str):
+    db_user = get_user_username(db, email)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if not verify_password(password, db_user.hashed_password):
+        raise HTTPException(status_code=404, detail="Invalid password")
     return db_user
