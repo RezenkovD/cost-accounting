f6daec196436b038b6281abbfe9ddb10d377f45f
RezenkovD <rezenkovdmitro@gmail.com>
2022-12-09 09:54:20 +0200

    Change endpoints with get user

--- src/app/routers/users.py
+++ src/app/routers/users.py
@@ -26,14 +26,6 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return users
 
 
-@router.get("/{user_id}/", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud_user.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
-@router.get("/me/", response_model=schemas.User)
-async def read_users_me(current_user: schemas.User = Depends(get_current_active_user)):
+@router.get("/read_me", response_model=schemas.User)
+def read_user(current_user: schemas.User = Depends(get_current_active_user)):
     return current_user
